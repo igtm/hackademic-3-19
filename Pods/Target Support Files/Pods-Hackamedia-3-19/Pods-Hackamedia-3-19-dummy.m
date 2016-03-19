3fa7190152cc5530ffe19f95d978d3eb4e78c1f6
miyass <aaaaaaaaaaaaaaaaa@google.com>
2016-03-19 11:15:56 +0900

    guess init

--- Pods/Target Support Files/Pods-Hackamedia-3-19/Pods-Hackamedia-3-19-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Hackamedia_3_19 : NSObject
-@end
-@implementation PodsDummy_Pods_Hackamedia_3_19
-@end
--- Pods/Target Support Files/Pods-Hackamedia-3-19UITests/Pods-Hackamedia-3-19UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Hackamedia_3_19UITests : NSObject
-@end
-@implementation PodsDummy_Pods_Hackamedia_3_19UITests
-@end
