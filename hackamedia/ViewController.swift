dc6860f4f3edc91d767cba46831bce6137ba8aa8
Tomokatsu Iguchi <tomo14916253@gmail.com>
2016-03-19 13:04:32 +0900

    Initial Commit

---
+++ hackamedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hackamedia
+//
+//  Created by 井口 智勝 on 2016/03/19.
+//  Copyright © 2016年 watnow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
