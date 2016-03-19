72235ee7770880438cfda163679ae3435d06e69d
Tomokatsu Iguchi <tomo14916253@gmail.com>
2016-03-20 02:46:44 +0900

    add nextGame_host nextGame_gest pushScore reserveScore

--- hackamedia/Server.swift
+++ hackamedia/Server.swift
@@ -14,6 +14,11 @@ class Server {
     private var _BASE_REF = Firebase(url: "\(BASE_URL)")
     private var _USER_REF = Firebase(url: "\(BASE_URL)/users")
     private var _ROOM_REF = Firebase(url: "\(BASE_URL)/rooms")
+    private var _RECORD_REF = Firebase(url: "\(BASE_URL)/records")
+    private var _ROOM_ID = "";
+    private var _GAME_ID = "";
+
+    private var _GAMES = [1,2,3,4];
     
     var BASE_REF: Firebase {
         return _BASE_REF
@@ -27,7 +32,11 @@ class Server {
         return _ROOM_REF
     }
     
-    func findOpponent(cb:(NSString,Int)->Void) {
+    var RECORD_REF: Firebase {
+        return _RECORD_REF
+    }
+    
+    func findOpponent(cb:(NSString,Int,Bool)->Void) {
         var key: String? = nil;
         var host: Int? = nil;
         _ROOM_REF.observeSingleEventOfType(.Value, withBlock: {
@@ -49,19 +58,61 @@ class Server {
             if key != nil {
                 // 既に待ってる人がいたので、gest登録して終了
                 self._BASE_REF.childByAppendingPath("rooms/" + key!).updateChildValues(["gest":USER_ID]);
-                cb(key!,host!);
+                self._ROOM_ID = key!; // room_idを保持
+                cb(key!,host!,false);
             }else{
                 // いないので、hostとして新しいroomを作る
-                var newRef = self._BASE_REF.childByAppendingPath("rooms").childByAutoId();
+                var newRef = self._ROOM_REF.childByAutoId();
                 newRef.setValue(["host":USER_ID]);
                 var newId = newRef.key;
                 self._ROOM_REF.childByAppendingPath(newId).observeEventType(.ChildAdded, withBlock: { snapshot in
                     
                     if snapshot.key == "gest" {
                         self._ROOM_REF.removeAllObservers();
-                        cb(newId,snapshot.value as! Int);
+                        self._ROOM_ID = newId; // room_idを保持
+                        cb(newId,snapshot.value as! Int,true);
                     }
                 })
+            }
+        })
+    }
+    
+    func nextGame_host(app_id: Int) ->Double{
+        let timestamp = NSDate().timeIntervalSince1970;
+        let inTenSec = timestamp + 1000 * 10;
+        print(timestamp);
+        print(inTenSec);
+        let newRef = _ROOM_REF.childByAppendingPath(_ROOM_ID).childByAppendingPath("games").childByAutoId();
+        newRef.setValue(["id":app_id,"startTime":inTenSec]);
+        _GAME_ID = newRef.key; // 新しいGame_id
+        return inTenSec;
+    }
+    
+    func nextGame_gest(cb:(Int,Int)->Void){
+        _ROOM_REF.childByAppendingPath(_ROOM_ID).childByAppendingPath("games").observeSingleEventOfType(.ChildAdded, withBlock: { snapshot in
+            let id = snapshot.value.objectForKey("id") as! Int;
+            let startTime = snapshot.value.objectForKey("startTime") as! Int;
+            self._GAME_ID = snapshot.key; // 新しいGame_id
+            print(id)
+            print(startTime)
+            cb(id,startTime);
+        })
+    }
+    
+    // スコアの送信
+    func pushScore(score:Int){
+        let newRef = _RECORD_REF.childByAppendingPath(_GAME_ID).childByAutoId();
+        newRef.setValue(["getter":USER_ID,"score":score]);
+    }
+    
+    // スコア変動の監視: nextGameの後,pushScoreが起きる前に呼び出す。
+    func reserveScore(cb:(Int)->Void){
+        let newRef = _RECORD_REF.childByAppendingPath(_GAME_ID).observeEventType(.ChildAdded, withBlock: { snapshot in
+            print(snapshot);
+            
+            if snapshot.value.objectForKey("getter") as! Int != USER_ID {
+                let score = snapshot.value.objectForKey("score") as! Int;
+                cb(score);
             }
         })
     }
--- hackamedia/ViewController.swift
+++ hackamedia/ViewController.swift
@@ -13,13 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var textfield1: UITextField!
     let ref = Firebase(url:BASE_URL)
     @IBAction func button1(sender: UIButton) {
-        let a = "test1";
-        textfield1.text = a;
-        ref.setValue(a)
+        Server.server.pushScore(10);
     }
     @IBAction func retrive(sender: UIButton) {
-        Server.server.findOpponent({(roomId:NSString,opponentId:Int) -> Void in
+        Server.server.findOpponent({(roomId:NSString,opponentId:Int,isHost:Bool) -> Void in
          print("マッチング成功: roomId: \(roomId) opponentId: \(opponentId)");
+            if isHost {
+                var startTime = Server.server.nextGame_host(1);
+                print("次のゲーム: id: 1 startTime: \(startTime)");
+                // 相手score変更の購読
+                Server.server.reserveScore({(score:Int)->Void in
+                    let a: Int? = Int(self.textfield1.text!);
+                    self.textfield1.text = String(score + a!);
+                })
+            }else{
+                Server.server.nextGame_gest({(id:Int,startTime:Int) -> Void in
+                    print("次のゲーム: id: \(id) startTime: \(startTime)");
+                    // 相手score変更の購読
+                    Server.server.reserveScore({(score:Int)->Void in
+                        let a: Int? = Int(self.textfield1.text!);
+                        self.textfield1.text = String(score + a!);
+                    })
+                });
+            }
         });
     }
     override func viewDidLoad() {
