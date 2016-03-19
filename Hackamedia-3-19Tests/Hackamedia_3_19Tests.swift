98251dfbf26d67cbdea3bbaf4cb320ea42929473
Tomokatsu Iguchi <tomo14916253@gmail.com>
2016-03-19 10:46:50 +0900

    Initial Commit

---
+++ Hackamedia-3-19Tests/Hackamedia_3_19Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Hackamedia_3_19Tests.swift
+//  Hackamedia-3-19Tests
+//
+//  Created by 井口 智勝 on 2016/03/19.
+//  Copyright © 2016年 watnow. All rights reserved.
+//
+
+import XCTest
+@testable import Hackamedia_3_19
+
+class Hackamedia_3_19Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
